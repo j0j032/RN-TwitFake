69ab2a0a62ac48241038ef765c8bb80465b8dfc5
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-25 19:07:21 +0200

    ✨ | TWRN-2 | NAV/LAYOUT | set Navigation and layout to individual pages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, Pressable, StyleSheet, View} from 'react-native';
 import Tweet from "../../components/Tweet";
 import tweets from '../../assets/data/tweets';
+import {Entypo} from "@expo/vector-icons";
+import {useRouter} from "expo-router";
 
 export default function TabOneScreen() {
+    const router = useRouter()
     return (
         <View style={styles.page} >
             <FlatList data={tweets} renderItem={({item}) => <Tweet tweet={item} />} />
+            <Pressable style={styles.floatingButton} onPress={() => router.push('/new-tweet')} >
+                <Entypo name="plus" size={30} color="white" />
+            </Pressable >
         </View >
     );
 }
@@ -14,5 +20,25 @@ const styles = StyleSheet.create({
     page: {
         flex: 1,
         backgroundColor: 'white',
+    },
+    floatingButton: {
+        backgroundColor: '#1C9BF0',
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     }
 });
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Image, StyleSheet} from "react-native";
-import {Text, View} from "./Themed";
+import {Image, Pressable, StyleSheet, Text, View} from "react-native";
 import {TweetType} from "../types";
 import {Entypo} from "@expo/vector-icons";
 import IconButton from "./IconButton";
+import {Link} from "expo-router";
 
 type TweetProps = {
     tweet: TweetType
@@ -11,25 +11,27 @@ type TweetProps = {
 
 const Tweet = ({tweet}: TweetProps) => {
     return (
-        <View style={styles.container} >
-            <Image source={{uri: tweet.user.image}} style={styles.userImage} />
-            <View style={styles.mainContainer} >
-                <View style={styles.tweetHeader} >
-                    <Text style={styles.name} >{tweet.user.name}</Text >
-                    <Text style={styles.username} >{tweet.user.username} - 2h</Text >
-                    <Entypo name="dots-three-horizontal" size={16} color="grey" style={{marginLeft: 'auto'}} />
+        <Link href={`/tweet/${tweet.id}`} asChild >
+            <Pressable style={styles.container} >
+                <Image source={{uri: tweet.user.image}} style={styles.userImage} />
+                <View style={styles.mainContainer} >
+                    <View style={styles.tweetHeader} >
+                        <Text style={styles.name} >{tweet.user.name}</Text >
+                        <Text style={styles.username} >{tweet.user.username} - 2h</Text >
+                        <Entypo name="dots-three-horizontal" size={16} color="grey" style={{marginLeft: 'auto'}} />
+                    </View >
+                    <Text style={styles.content} >{tweet.content}</Text >
+                    {tweet.image ? <Image source={{uri: tweet.image}} style={styles.image} /> : null}
+                    <View style={styles.footer} >
+                        <IconButton iconName="comment" text={tweet.numberOfComments} />
+                        <IconButton iconName="retweet" text={tweet.numberOfComments} />
+                        <IconButton iconName="heart" text={tweet.numberOfComments} />
+                        <IconButton iconName="chart" text={tweet.numberOfComments} />
+                        <IconButton iconName="share-apple" />
+                    </View >
                 </View >
-                <Text style={styles.content} >{tweet.content}</Text >
-                {tweet.image ? <Image source={{uri: tweet.image}} style={styles.image} /> : null}
-                <View style={styles.footer} >
-                    <IconButton iconName="comment" text={tweet.numberOfComments} />
-                    <IconButton iconName="retweet" text={tweet.numberOfComments} />
-                    <IconButton iconName="heart" text={tweet.numberOfComments} />
-                    <IconButton iconName="chart" text={tweet.numberOfComments} />
-                    <IconButton iconName="share-apple" />
-                </View >
-            </View >
-        </View >
+            </Pressable >
+        </Link >
     );
 };
 
