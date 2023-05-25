2d65f51a68bb8c27d95180a17d9570c023750373
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-25 19:54:15 +0200

    🚧 | TWRN-3 | DRAWER NAV | init Drawer nav

---
+++ app/(drawer)/twitter-blue.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const TwitterBlue = () => {
+    return (
+        <View >
+            <Text >TwitterBlue</Text >
+        </View >
+    );
+};
+
+export default TwitterBlue;
